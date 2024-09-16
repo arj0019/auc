4f92437c8085bdb8f4ae6e719ee64ffd13405040
Reid Johnson <arj0019@auburn.edu>
2024-09-15 21:49:55 -0500

    refactored parser for simplicity
    the re package uses dicts for groups that are structured well for an ast. removed class implementations for (non-)terminals, and utilized tuples. removed support for alternative (non-)terminal definitions; need a better solution for naming.

--- ecc.py
+++ ecc.py
@@ -11,35 +11,15 @@ class Parser():
   def parse(self, source):
     loc = 0
     while loc < len(source):
-      for non_terminal in self.non_terminals:
-        match = re.match(non_terminal.expression, source[loc:])
+      for symbol, expression in self.non_terminals:
+        match = re.match(expression, source[loc:])
         if match:
-          print(f"{non_terminal.__name__} := {match.groupdict()}")
+          print(f"{symbol} := {match.groupdict()}")
           loc = match.end(); break
       else: loc += 1  # skip unrecognized characters
         
   @staticmethod
   def _create_non_terminals(grammar):
     grammar = re.sub(r'\s*\n\s*', ' ', grammar)
-    grammar = [(symbol, re.split(r'\s*\|\s*', expressions))
-               for (symbol, expressions)
-               in re.findall(BNF, grammar)]
-    
-    non_terminals = []
-    for symbol, expressions in grammar:
-      base_non_terminal = type(symbol,
-                               (object,),
-                               {'expression':expressions[0]})
-
-      if (len(expressions) > 1):
-        for expression in expressions:
-          args = re.findall(r'\w+', expression)
-          _symbol = symbol + '_' + '_'.join(args)
-          non_terminals.append(type(_symbol,
-                                    (base_non_terminal,),
-                                    {'expression':expression,
-                                     'val':None}))
-      else: non_terminals.append(base_non_terminal)
-
-    return non_terminals
+    return re.findall(BNF, grammar)
 
