6b5c1b7d28f93dbb89ccc40b92dc7ec23acefbd3
Reid Johnson <arj0019@auburn.edu>
2024-09-24 01:35:31 -0500

    added del pre-processing grammar

--- lang/t.s
+++ lang/t.s
@@ -1,11 +1,11 @@
+.del (\n|\t)
+
 <expression> ::= (?P<type>\w+)\s+(?P<identifier>\w+)\s*=\s*(?P<value>\w+)(?P<delimeter>.)
 
 <delimeter> ::= ;
 
 <type> ::= int
 
-<identifier> ::= [A-Za-z_][0-9A-Za-z_]*
-
 <value> ::= -?[0-9]+
 
-<whitespace> ::= \s+
+<identifier> ::= [A-Za-z_][0-9A-Za-z_]*
