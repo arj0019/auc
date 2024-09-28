fa4aa0ab1e4ebca803cabf4c475572f73d3a2258
Reid Johnson <arj0019@auburn.edu>
2024-09-28 16:18:45 -0500

    added ast reduction
    recursively reduce nested lists and dictionaries

--- ecc.py
+++ ecc.py
@@ -13,14 +13,52 @@ BNF = r'<(\w+)>\s*::=\s*(.*?)\s*(?=<\w+>\s*::=|$)'
 
 
 class Parser():
-  def __init__(self, grammar):
+  def __init__(self, grammar:str, **kwargs):
+    """ Initialize a parser from a given grammar and configuation.
+
+    Args:
+      grammar (str): lanugage grammar formatted as BNF+RE
+
+    KwArgs:
+      simplify (bool): reduce nested lists and dictionaries
+    """
     self.grammar = self._parse_grammar(grammar)
+    self.simplify = kwargs.get('simplify', False)
+
+  def _parse_grammar(self, grammar):
+    match = re.search(DEL, grammar, re.MULTILINE)
+    self._del = match.group(1) if match else ''
+
+    grammar = re.sub(r'\s*\n\s*', ' ', grammar)
+    return OrderedDict(re.findall(BNF, grammar))
 
   def parse(self, source):
+    """ Parse the given source code into an AST with recursive decent.
+
+    Args:
+      source (str): source code to parse; formatted according to grammar
+
+    Returns:
+      ast (list, dict): abstract syntax tree of the given source code
+    """
     _source = self.preprocess(source)
-    return self._parse(_source, self.grammar.items())
+    ast = self._parse(_source, self.grammar.items())
+    if self.simplify: ast = self._simplify(ast)
+    return ast
+
+  def preprocess(self, source):
+    return re.sub(self._del, '', source)  # remove grammar exclusions
 
   def _parse(self, source, targets):
+    """ Parse the given source code into an AST with recursive decent.
+
+    Args:
+      source (str): source code to parse; formatted according to grammar
+      targets (dict): targeted symbol(s) of recursive decent
+
+    Returns:
+      ast (list, dict): abstract syntax tree of the given source code
+    """
     ast = []
     while source:  # sequentially match source to grammar
       for symbol, expression in targets:
@@ -34,29 +72,42 @@ class Parser():
           for _symbol, _expression in _match:
             if not _expression: continue
             parsed_match[symbol][_symbol] = self._parse(_expression, {_symbol: self.grammar[_symbol]}.items())
-        else: parsed_match[symbol] = match.group(0)
-        
+        else:
+          parsed_match[symbol] = match.group(0)
+
         ast.append(parsed_match); break
 
       else: raise SyntaxError(f"@ln:col ({source[:30]})")
 
-    # flatten single element lists and terminal values
-    if (len(ast) == 1):
-      if (len(ast[0]) == 1): ast = match.group(0)
-      else: ast = ast[0]
-
     return ast
 
-  def preprocess(self, source):
-    source = re.sub(self._del, '', source)  # remove grammar exclusions
-    return source
+  def _simplify(self, struct):
+    """ Recursively reduce nested lists and dictionaries.
 
-  def _parse_grammar(self, grammar):
-    match = re.search(DEL, grammar, re.MULTILINE)
-    self._del = match.group(1) if match else ''
+    Reduce any list with a single element to that element.
+    
+    In any dictionary, if the value corresping to a key is a dictionary with
+    only one key, and that key is the same as the outer key, reduce the value
+    corresonding to the outer key to the value corresponding to the inner key.
+    """
+    if isinstance(struct, list):
+      # recursively reduce lists by element(s)
+      _struct = [self._simplify(item) for item in struct]
 
-    grammar = re.sub(r'\s*\n\s*', ' ', grammar)
-    return OrderedDict(re.findall(BNF, grammar))
+      # if a list has one element, replace it with that element
+      if len(_struct) == 1: return _struct[0]
+      else: return _struct
+
+    elif isinstance(struct, dict):
+        _struct = {}
+        for key, value in struct.items(): # Recursively reduce dictionaries by key repetition
+          _value = self._simplify(value)
+          if (isinstance(_value, dict) and len(_value) == 1 and key in _value):
+              _struct[key] = _value[key]
+          else: _struct[key] = _value
+        return _struct
+
+    else: return struct
 
 
 def printh(header, body): print(f"--- {header} {'-' * (TERMSIZE.columns - len(header) - 5)}\n{body}")
@@ -66,13 +117,14 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser(prog='ecc', description='x86 assembly compilers generated from Backus Naur grammar extended with regular expressions')
   parser.add_argument('grammar', help='language grammar file path')
   parser.add_argument('source', help='source code file path')
+  parser.add_argument('-s', '--simplify', action='store_true', help='simplify the ast')
   parser.add_argument('-v', '--verbose', action='store_true', help='enable verbose output')
 
   args = parser.parse_args()
 
   with open(args.grammar, 'r') as file:
     _grammar = file.read()
-  parser = Parser(_grammar)
+  parser = Parser(_grammar, simplify=args.simplify)
   if (args.verbose): printh('GRAMAMR', pprint.pformat(dict(parser.grammar), sort_dicts=False))
 
   with open(args.source, 'r') as file:
