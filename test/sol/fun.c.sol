cf1ee769aab78b116addc8844a02c056ea8341e3
Reid Johnson <arj0019@auburn.edu>
2024-10-08 00:08:26 -0500

    added 'complex' function routine test program

---
+++ test/sol/fun.c.sol
@@ -0,0 +1,71 @@
+[
+  {
+    "function": {
+      "type": [
+        {
+          "type": "int"
+        }
+      ],
+      "identifier": [
+        {
+          "identifier": "main"
+        }
+      ],
+      "routine": [
+        {
+          "routine": {
+            "expression": [
+              {
+                "expression": {
+                  "type": [
+                    {
+                      "type": "int"
+                    }
+                  ],
+                  "identifier": [
+                    {
+                      "identifier": "a"
+                    }
+                  ],
+                  "op": [
+                    {
+                      "op": "="
+                    }
+                  ],
+                  "expression": [
+                    {
+                      "expression": {
+                        "value": [
+                          {
+                            "value": "0"
+                          }
+                        ]
+                      }
+                    }
+                  ]
+                }
+              }
+            ],
+            "routine": [
+              {
+                "routine": {
+                  "return": [
+                    {
+                      "return": {
+                        "value": [
+                          {
+                            "value": "0"
+                          }
+                        ]
+                      }
+                    }
+                  ]
+                }
+              }
+            ]
+          }
+        }
+      ]
+    }
+  }
+]
